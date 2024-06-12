da5e1999d52bfc9a57c552bd80dd6ed213abcffe
Hedayat Farahi <hedayatfarahi8@gmail.com>
2024-06-12 20:24:09 +0200

    send the customer and products in orders

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,62 +1,66 @@
 // orders.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Order = require('../models/order');
-const User = require('../models/user')
+const Order = require("../models/order");
+const User = require("../models/user");
 
-router.post('/', async (req, res) => {
-    try {
-        const { customer, delivery, products, totalPrice, payment, time } = req.body;
+router.post("/", async (req, res) => {
+  try {
+    const { customer, delivery, products, totalPrice, payment, time } =
+      req.body;
 
-        let user = await User.findOne({ phone: customer.phone });
+    let user = await User.findOne({ phone: customer.phone });
 
-        if (!user) {
-            // Create a new user object omitting the email if not provided
-            const userData = {
-                name: customer.name,
-                phone: customer.phone,
-                // Include email only if it's provided in the payload
-                ...(customer.email && { email: customer.email }),
-                address: {
-                    street: delivery.street,
-                    postcode: delivery.postcode,
-                    floor: delivery.floor,
-                },
-            };
+    if (!user) {
+      // Create a new user object omitting the email if not provided
+      const userData = {
+        name: customer.name,
+        phone: customer.phone,
+        // Include email only if it's provided in the payload
+        ...(customer.email && { email: customer.email }),
+        address: {
+          street: delivery.street,
+          postcode: delivery.postcode,
+          floor: delivery.floor,
+        },
+      };
 
-            user = new User(userData);
-            await user.save();
-        }
-
-        // Calculate the order number based on the current number of orders
-        const ordersCount = await Order.countDocuments();
-        const orderNumber = `${1000 + ordersCount}`;
-
-        // Create a new Order document using the Order model
-        const newOrder = new Order({
-            orderNumber,
-            status: 'pending', // Set the default status to 'pending'
-            customer: user._id, // Connect the order to the user
-            delivery,
-            products,
-            totalPrice,
-            payment,
-            time,
-        });
-
-        // Save the new order to the database
-        const savedOrder = await newOrder.save();
-
-        res.status(201).json(savedOrder);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+      user = new User(userData);
+      await user.save();
     }
+
+    // Calculate the order number based on the current number of orders
+    const ordersCount = await Order.countDocuments();
+    const orderNumber = `${1000 + ordersCount}`;
+
+    // Create a new Order document using the Order model
+    const newOrder = new Order({
+      orderNumber,
+      status: "pending", // Set the default status to 'pending'
+      customer: user._id, // Connect the order to the user
+      delivery,
+      products,
+      totalPrice,
+      payment,
+      time,
+    });
+
+    // Save the new order to the database
+    const savedOrder = await newOrder.save();
+
+    res.status(201).json(savedOrder);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Get all orders
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find().populate("customer").populate({
+      path: "products.productId",
+      model: "Product",
+    });
     res.status(200).json(orders);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -64,11 +68,11 @@ router.get('/', async (req, res) => {
 });
 
 // Get a single order by ID
-router.get('/:orderNumber', async (req, res) => {
+router.get("/:orderNumber", async (req, res) => {
   try {
     const order = await Order.findOne({ orderNumber: req.params.orderNumber });
     if (!order) {
-      return res.status(404).json({ error: 'Order not found' });
+      return res.status(404).json({ error: "Order not found" });
     }
     res.status(200).json(order);
   } catch (error) {
@@ -77,13 +81,13 @@ router.get('/:orderNumber', async (req, res) => {
 });
 
 // Update an order by ID
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
     if (!order) {
-      return res.status(404).json({ error: 'Order not found' });
+      return res.status(404).json({ error: "Order not found" });
     }
     res.status(200).json(order);
   } catch (error) {
@@ -92,20 +96,16 @@ router.put('/:id', async (req, res) => {
 });
 
 // Delete an order by ID
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const order = await Order.findByIdAndDelete(req.params.id);
     if (!order) {
-      return res.status(404).json({ error: 'Order not found' });
+      return res.status(404).json({ error: "Order not found" });
     }
     res.status(204).send();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-
-
-
-
 
 module.exports = router;
